d3f78e5bdfe522da49ea9a20035378f1d16281ae
Jason Byrne <jason.byrne@oddball.io>
2023-10-05 08:56:56 -0400

    update genres

--- devfest/models/dim_genres.sql
+++ devfest/models/dim_genres.sql
@@ -2,10 +2,13 @@ WITH final AS (
     SELECT 
         {{ generate_id('genre') }} AS genre_id
         , genre AS genre_name 
-        , year
-        , rank
-        , global_sales
+        , MIN(year) AS first_year
+        , MAX(year) AS last_year
+        , MIN(rank) AS best_rank
+        , COUNT(1) AS games_count
+        , SUM(global_sales) AS total_sales
     FROM {{ ref('raw__sales') }}
+    GROUP BY genre_id, genre_name
 )
 
 SELECT * FROM final
